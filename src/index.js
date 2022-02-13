ff8882be2d90077973d0329cd3dee4960fd9a2f5
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-02-13 22:16:05 +0200

    task completed

--- src/index.js
+++ src/index.js
@@ -9,106 +9,28 @@ const DEBOUNCE_DELAY = 300;
 const inputSearchRef = document.querySelector('#search-box');
 inputSearchRef.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
+const renderCountries = template => {
+  const countriesListRef = document.querySelector('.country-list');
+  countriesListRef.innerHTML = template;
+};
+
 function handleInput() {
-  if (inputSearchRef.value.length < 2) {
-    return console.log('Too many maches found, please enter more specific name');
+  let countryName = inputSearchRef.value.trim();
+  if (countryName.length < 2) {
+    return (
+      renderCountries(''),
+      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+    );
   } else {
-    fetchCountries(inputSearchRef.value)
+    fetchCountries(countryName)
       .then(countries => {
         const countriesTemplate = createCountriesList(countries);
-        console.log(countries);
         renderCountries(countriesTemplate);
       })
-      .catch(error => console.log(error));
+      .catch(error => {
+        if (error.status === 404)
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        console.log(error);
+      });
   }
-
-  const renderCountries = template => {
-    const countriesListRef = document.querySelector('.country-list');
-    countriesListRef.innerHTML = template;
-  };
-
-  // function handleInput() {
-  //   if (inputSearchRef.value.length < 2) {
-  //     return console.log('Too many maches found, please enter more specific name');
-  //   } else {
-  //     fetchCountries(inputSearchRef.value)
-  //       .then(countries => console.log(countries))
-  //       .catch(error => console.log(error));
-  //     // console.log(inputSearchRef.value);
-  //   }
-
-  //   const email = form.email.value;
-  //   const message = form.message.value;
-
-  //   console.log(`Email: ${email}, message: ${message}`);
-
-  //   localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
 }
-
-// const fetchUsersBtn = document.querySelector('.btn');
-// const userList = document.querySelector('.user-list');
-
-// fetchUsersBtn.addEventListener('click', () => {
-//   fetchUsers()
-//     .then(users => renderUserList(users))
-//     .catch(error => console.log(error));
-// });
-
-// function renderUserList(users) {
-//   const markup = users
-//     .map(user => {
-//       return `<li>
-//           <p><b>Name</b>: ${user.name}</p>
-//           <p><b>Email</b>: ${user.email}</p>
-//           <p><b>Company</b>: ${user.company.name}</p>
-//         </li>`;
-//     })
-//     .join('');
-//   userList.innerHTML = markup;
-// }
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const {
-//     elements: { email, message },
-//   } = event.currentTarget;
-
-//   if (email.value === '' || message.value === '') {
-//     return alert('Please fill in all the fields!');
-//   }
-
-//   console.log(`Email: ${email.value}, message: ${message.value}`);
-//   event.currentTarget.reset();
-//   localStorage.removeItem('feedback-form-state');
-// }
-
-// const fetchUsersBtn = document.querySelector('.btn');
-// const userList = document.querySelector('.user-list');
-
-// fetchUsersBtn.addEventListener('click', () => {
-//   fetchUsers()
-//     .then(users => renderUserList(users))
-//     .catch(error => console.log(error));
-// });
-
-// function fetchUsers() {
-//   return fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
-// function renderUserList(users) {
-//   const markup = users
-//     .map(user => {
-//       return `<li>
-//           <p><b>Name</b>: ${user.name}</p>
-//           <p><b>Email</b>: ${user.email}</p>
-//           <p><b>Company</b>: ${user.company.name}</p>
-//         </li>`;
-//     })
-//     .join('');
-//   userList.innerHTML = markup;
-// }
--- src/js/api/countriesList.js
+++ src/js/api/countriesList.js
@@ -1,18 +1,33 @@
-const countryCard = country => {
+const countryBigCard = country => {
   const { name, flags, capital, population, languages } = country;
-  //   const timeZones = timezones.join(', ');
 
   return `
-    <div class="country">
-      <img src=${flags.png}>
-      <h2>${name}</h2>
-      <div>Capital: ${capital}</div>
-      <h3>Population: ${population}</h3>
-      <h3>Language: ${languages}</h3>
+    <div class="country-card">
+      <img src=${flags.svg} class="country-card_img">
+      <h2>${name.official}</h2>
+      </div>
+      <div class="country-card_big">
+      <h3>Capital: <span class="country-card_desc">${capital}</span></h3>
+      <h3>Population: <span class="country-card_desc">${population}</span></h3>
+      <h3>Language: <span class="country-card_desc">${Object.values(languages)}</span></h3>
+    </>
+  `;
+};
+
+const countrySmallCard = country => {
+  const { name, flags } = country;
+
+  return `
+    <div class="country-card">
+    <img src=${flags.svg} class="country-card_img">
+    <h2>${name.official}</h2>    
     </div>
   `;
 };
 
 export const createCountriesList = countries => {
-  return countries.map(country => countryCard(country)).join('');
+  if (countries.length === 1) {
+    return countries.map(country => countryBigCard(country)).join('');
+  }
+  return countries.map(country => countrySmallCard(country)).join('');
 };
